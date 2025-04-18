aeb363524dd62fe1d8f3ac58056622a6fa7973f5
SSSycamore <2083551494@qq.com>
2025-04-18 23:50:01 +0800

    新增菜品和起售停售菜品功能

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -1,9 +1,12 @@
 package com.sky.controller.admin;
 
+import com.sky.result.Result;
 import com.sky.service.SetmealService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class SetmealController {
     @Autowired
     private SetmealService setmealService;
+
+    @PostMapping
+    @ApiOperation(value = "新增套餐")
+    public Result save()
 }
